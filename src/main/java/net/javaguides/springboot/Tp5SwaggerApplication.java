9ae4c093737508881daf43218521843d1f39d9b8
DESKTOP-0FE37AA\eyach <eyachaabeni2000@gmail.com>
2023-12-21 13:59:13 +0100

    tp5

---
+++ src/main/java/net/javaguides/springboot/Tp5SwaggerApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5SwaggerApplication.class, args);
+	}
+
+}
